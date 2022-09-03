c542506bf84bfea351da8084f7ecf6266ddb11df
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2022-09-03 00:19:16 -0300

    Add Tailwind and Eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:prettier/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -17,8 +18,9 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "prettier"],
+  plugins: ["react", "prettier", "tailwindcss"],
   rules: {
+    "react/prop-types": "off",
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
     "prettier/prettier": [
@@ -28,6 +30,7 @@ module.exports = {
         tabWidth: 2,
         semi: true,
         singleQuote: false,
+        printWidth: 80,
       },
     ],
     "react/self-closing-comp": "warn",
